116299442e2e727924862c80e1d8906c47f82cca
Ubuntu <ubuntu@ip-172-31-84-204.ec2.internal>
2020-05-12 02:21:36 +0000

    added database

--- app/models.py
+++ app/models.py
@@ -31,7 +31,7 @@ class Users(db.Model):
     
     def __init__(self, username, password, first_name, last_name, email, location, biography, profile_photo, joined):
         self.username = username
-        self.password = generate_password_hash(password)
+        self.password = generate_password_hash(password, method='pbkdf2:sha256')
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
